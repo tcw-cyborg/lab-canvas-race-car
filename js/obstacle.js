0689d91b8417e7b0baffb1b5b7e3e7f11d399d87
Projetphp <alexandre.capaldi@hotmail.fr>
2021-03-20 16:29:31 +0100

    iteration 4

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,17 +1,41 @@
-function random(from, to) {
-  // TODO
+function random(minWidth, maxWidth) {
+
+
+  Math.floor(Math.random * (maxWidth - minWidth + 1) + minWidth)
+
+  var minGap = 50;
+  var maxGap = 200;
+  // tirer au hasard entre 50 et 200
+  var gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+
 }
 
 class Obstacle {
-  constructor() {
-    // TODO
+  constructor(width, height, x, y) {
+
+    this.w = width;
+    this.h = height;
+    this.x = x;
+    this.y = y;
+
   }
 
   draw() {
-    // TODO
-  }
+    var W = canvas.width; // 480
+    var H = canvas.height; // 270
 
-  hits(car) {
-    // TODO
+    const myObstacles = [];
+
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, this.w, this.height);
+    ctx.closePath();
+
+    myObstacles.push(new Obstacle(width, 10, W, 0);
+
+    }
+
+    hits(car) {
+      // TODO
+    }
   }
-}
