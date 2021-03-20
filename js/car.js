5e7ee1f6659dbf9c5c8d180edd70878ead360abb
Projetphp <alexandre.capaldi@hotmail.fr>
2021-03-20 16:01:09 +0100

    iteration 2

--- js/car.js
+++ js/car.js
@@ -4,17 +4,20 @@ class Car {
     img.onload = () => {
       this.img = img;
 
-      const imgRatio = img.naturalWidth/img.naturalHeight;
 
-      // TODO
+      const imgRatio = img.naturalWidth / img.naturalHeight;
+
+      this.w = 100;
+      this.h = this.w / imgRatio;
+      this.x = W / 2 - this.w / 2;
+      this.y = H - this.h;
     }
     img.src = "images/car.png";
   }
 
   draw() {
     if (!this.img) return; // if `this.img` is not loaded yet => don't draw
-
-    // TODO
+    ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
   }
 
   moveLeft() {
--- js/main.js
+++ js/main.js
@@ -12,13 +12,42 @@ function draw() {
   // Iteration 1: road drawing
   //
 
-  // TODO
+
+  //
+  // Iteration 1: road drawing
+  ctx.beginPath();
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, 1000, 1600);
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.fillStyle = "gray";
+  ctx.fillRect(100, 0, 800, 1600);
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.fillRect(120, 0, 30, 1600);
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.fillRect(850, 0, 30, 1600);
+  ctx.closePath();
+
+  let y = 0;
+  for (let i = 0; i < 15; i++) {
+    y += 120;
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.fillRect(495, y, 10, 60);
+    ctx.closePath();
+  }
 
   //
   // Iteration 2: car drawing
   //
-
-  // TODO
+  car.draw()
 
   //
   // Iteration #4: obstacles
@@ -48,6 +77,7 @@ document.onkeydown = function (e) {
 
 let raf;
 let frames = 0;
+
 function animLoop() {
   frames++;
 
@@ -63,12 +93,12 @@ function startGame() {
     cancelAnimationFrame(raf);
   }
 
-  // TODO
+  car = new Car()
 
   animLoop();
 }
 
-document.getElementById("start-button").onclick = function() {
+document.getElementById("start-button").onclick = function () {
   startGame();
 };
 
